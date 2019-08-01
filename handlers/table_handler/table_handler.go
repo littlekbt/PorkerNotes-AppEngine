ddf00bff94f87f554f98a70f5bfd4ae974a43bf5
littlekbt <kr.kubota.11@gmail.com>
2019-08-01 23:42:12 +0900

    post endpoint docker-compose

--- handlers/table_handler/table_handler.go
+++ handlers/table_handler/table_handler.go
@@ -1,16 +1,43 @@
 package table_handler
 
 import (
+	"bytes"
+	"database/sql"
 	"encoding/json"
+	"io"
+	"log"
 	"net/http"
 
-	//"github.com/littlekbt/PorkerNotes-AppEngine/models/response"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/littlekbt/PorkerNotes-AppEngine/models/table"
 )
 
 func Handle(w http.ResponseWriter, r *http.Request) {
-	tables := make([]table.Table, 1)
-	tables[0] = table.Table{ID: int64(1)}
+	tables := []table.Table{}
+	switch r.Method {
+	case "GET":
+		tables = append(tables, table.Table{ID: int64(1)})
+	case "POST":
+		body := r.Body
+		defer body.Close()
+
+		buf := new(bytes.Buffer)
+		io.Copy(buf, body)
+		var t table.Table
+		json.Unmarshal(buf.Bytes(), &t)
+
+		db, err := sql.Open("mysql", "root@tcp(127.0.0.1:3306)/porker_notes")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer db.Close()
+
+		ins, err := db.Prepare("INSERT INTO tables(name) VALUES(?)")
+		if err != nil {
+			log.Fatal(err)
+		}
+		ins.Exec(t.Name)
+		tables = append(tables, t)
+	}
 	json.NewEncoder(w).Encode(tables)
-	//json.NewEncoder(w).Encode(response.Response{Status: "ok", Message: "Hello world."})
 }
--- models/table/table.go
+++ models/table/table.go
@@ -3,5 +3,6 @@ package table
 import ()
 
 type Table struct {
-	ID int64 `json:"id,int" db:"id, primarykey"`
+	ID   int64  `json:"id" db:"id, primarykey"`
+	Name string `json:"name" db:"name"`
 }
